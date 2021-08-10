f9987e06d51b3de89c81545cabf177f3952f84c2
Daiius <yamashita.daiji@gmail.com>
2021-08-11 02:39:03 +0900

    add keyboard hook codes

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
 namespace VolumeShortcut
 {
     /// <summary>
@@ -13,6 +16,23 @@ namespace VolumeShortcut
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        KeyboardHooker hooker = new KeyboardHooker();
+        System.Windows.Forms.KeysConverter converter = new System.Windows.Forms.KeysConverter();
+
+        bool isScrollLockPressed = false;
+
+        MainWindow window = new MainWindow();
+        IntPtr handle;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -28,20 +48,48 @@ namespace VolumeShortcut
                 Text = "Volume Shortcut"
             };
 
+            handle = new WindowInteropHelper(window).Handle;
+
             notifyIcon.MouseClick += NotifyIcon_MouseClick;
+
+            hooker.KeyDownEvent += Hooker_KeyDownEvent;
+            hooker.KeyUpEvent += Hooker_KeyUpEvent;
+            hooker.Hook();
+        }
+
+        private void Hooker_KeyUpEvent(object sender, KeyEventArgs e)
+        {
+            //var str = converter.ConvertToString(e.KeyCode);
+            if (e.KeyCode == 145 || e.KeyCode == 45) isScrollLockPressed = false;
+        }
+
+        private void Hooker_KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            //var str = converter.ConvertToString(e.KeyCode) + " Down";
+            if (e.KeyCode == 145 || e.KeyCode == 45) isScrollLockPressed = true;
+
+            if (isScrollLockPressed && e.KeyCode == 101)
+            {
+                SendMessageW(handle, WM_APPCOMMAND, handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+            }
+            else if (isScrollLockPressed && e.KeyCode == 40)
+            {
+                SendMessageW(handle, WM_APPCOMMAND, handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+            }
         }
 
         private void NotifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
-                var window = new MainWindow();
+                
                 window.Show();
             }
         }
 
         private void Exit_Click(object sender, EventArgs e)
         {
+            hooker.UnHook();
             Shutdown();
         }
     }
---
+++ KeyEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeShortcut
+{
+    public class KeyEventArgs : EventArgs
+    {
+        public int KeyCode { get; }
+
+        public KeyEventArgs(int keyCode)
+        {
+            KeyCode = keyCode;
+        }
+    }
+}
