5a8cdff6ed6c04fd4009a88bb96646861f4e5c42
Daiius <yamashita.daiji@gmail.com>
2021-08-11 01:52:26 +0900

    add notify icon codes

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,36 @@ namespace VolumeShortcut
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("outline_keyboard_alt_white_48dp.ico", UriKind.Relative)).Stream;
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("修了", null, Exit_Click);
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = "Volume Shortcut"
+            };
+
+            notifyIcon.MouseClick += NotifyIcon_MouseClick;
+        }
+
+        private void NotifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                var window = new MainWindow();
+                window.Show();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
     }
 }
